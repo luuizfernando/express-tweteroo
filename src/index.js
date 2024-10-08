57f6abd2f88023c0c2d82fc308df7b91feebde5f
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-10-08 15:12:08 -0300

    Post de Users

--- src/index.js
+++ src/index.js
@@ -8,25 +8,35 @@ app.use(json());
 
 const PORT = 5000;
 
+const users = [];
 const tweets = [];
 
-app.get("/tweets", (req, res) => {
-    res.send(tweets);
-});
 
 app.post("/sign-up", (req, res) => {
-
+    const { username, avatar } = req.body;
+    
+    if (!username || !avatar) return res.status(422).send("Todos os campos são obrigatórios!!");
+    
+    const novoUsuario = { username: username, avatar: avatar };
+    
+    users.push(novoUsuario);
+    res.sendStatus(201);
+    
 });
 
 app.post("/tweets", (req, res) => {
     const { username, tweet } = req.body;
-
+    
     if (!username || !tweet) return res.status(422).send("Todos os campos são obrigatórios!!");
-
-    const novoTweet = {username: username, tweet: tweet};
-
+    
+    const novoTweet = { username: username, tweet: tweet };
+    
     tweets.push(novoTweet);
     res.sendStatus(201);
+});
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets);
 });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}.`));
