41b8b67755d1668a94696b34b0aefe0832337bed
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-09 11:55:34 -0300

    POST de tweets terminado

--- src/index.js
+++ src/index.js
@@ -11,28 +11,29 @@ const PORT = 5000;
 const users = [];
 const tweets = [];
 
-
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
-    
+
     if (!username || !avatar) return res.status(422).send("Todos os campos são obrigatórios!!");
-    
+
     const novoUsuario = { username: username, avatar: avatar };
-    
+
     users.push(novoUsuario);
     res.sendStatus(201);
-    
 });
 
 app.post("/tweets", (req, res) => {
+
+    if (users.length == 0) return res.send("UNAUTHORIZED");
+
     const { username, tweet } = req.body;
-    
+
     if (!username || !tweet) return res.status(422).send("Todos os campos são obrigatórios!!");
-    
+
     const novoTweet = { username: username, tweet: tweet };
-    
+
     tweets.push(novoTweet);
-    res.sendStatus(201);
+    res.status(201).send("OK");
 });
 
 app.get("/tweets", (req, res) => {
